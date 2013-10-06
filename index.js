829ccee650b99bd26891e7ed7eafcfff3bde1a98
Oleg Elifantiev <oleg@elifantiev.ru>
2013-10-06 15:17:10 +0400

    Initial project state

---
+++ index.js
@@ -0,0 +1,8 @@
+var libPath = process.env('COVER') == 'CHUNKING' ? '/lib-cov' : '/lib';
+
+module.exports = {
+   Chunker: require(libPath + '/chunker'),
+   LineCounter: require(libPath + '/line-counter'),
+   GZipChunker: require(libPath + '/gzip-chunker'),
+   S3MultipartUploader: require(libPath + '/s3-multipart')
+};
