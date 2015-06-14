61f607d6e291d1ed320c7d21014895b5978ed67f
Oleg Elifantiev <oleg@elifantiev.ru>
2015-06-14 23:11:11 +0300

    SizeChunker: No more unnecessary empty chunks emitted
    Previously, when buffer is received, which size is equals to a remainder, required to finish current chunk, unnecessary empty chunk was emitter.
    
    Fixes: #7

--- test/test-size-chunker.js
+++ test/test-size-chunker.js
@@ -36,16 +36,16 @@ describe('SizeChunker', function () {
 
       it('is emitted, when flushTail is set', function (done) {
 
-         var s = new Chunker({ chunkSize: 10, flushTail: true }), fixture = [
+         var s = new Chunker({chunkSize: 10, flushTail: true}), fixture = [
             'aaaaaaaaaa', 'bbbbbbbbbb', 'cccccccccc', 'dd'
          ], buf = '', i = 0;
 
-         s.on('chunkEnd',function (chunkNum, d) {
+         s.on('chunkEnd', function (chunkNum, d) {
             assert.equal(buf, fixture[chunkNum]);
             i++;
             buf = '';
             d();
-         }).on('data',function (chunk) {
+         }).on('data', function (chunk) {
             buf += chunk.data.toString();
          }).on('end', function () {
             assert.equal(fixture.length, i);
@@ -68,16 +68,16 @@ describe('SizeChunker', function () {
 
       it('is ignored, when flushTail is not set (default is false)', function (done) {
 
-         var s = new Chunker({ chunkSize: 10 }), fixture = [
+         var s = new Chunker({chunkSize: 10}), fixture = [
             'aaaaaaaaaa', 'bbbbbbbbbb', 'cccccccccc'
          ], buf = '', i = 0;
 
-         s.on('chunkEnd',function (chunkNum, d) {
+         s.on('chunkEnd', function (chunkNum, d) {
             assert.equal(buf, fixture[chunkNum]);
             i++;
             buf = '';
             d();
-         }).on('data',function (chunk) {
+         }).on('data', function (chunk) {
             buf += chunk.data.toString();
          }).on('end', function () {
             assert.equal(fixture.length, i);
@@ -94,6 +94,36 @@ describe('SizeChunker', function () {
 
          setTimeout(function () {
             s.end('cdd');
+         }, 10);
+
+      });
+
+   });
+
+   describe('Regression testing', function () {
+
+      it('#7. No unnecessary 0-sized chunks are emitted', function(done){
+
+         var s = new Chunker({ chunkSize: 10, flushTail: false }),
+             fixture = [ 'aaaaaaaaaa', 'bbbbbbbbbb', 'cccccccccc' ], i = 0;
+
+         s.on('data',function (chunk) {
+            assert.equal(chunk.data.toString(), fixture[i++]);
+         }).on('end', function () {
+            assert.equal(fixture.length, i);
+            done()
+         });
+
+         setTimeout(function () {
+            s.write('aaaaaaaaaa');
+         }, 10);
+
+         setTimeout(function () {
+            s.write('bbbbbbbbbb');
+         }, 10);
+
+         setTimeout(function () {
+            s.end('cccccccccc');
          }, 10);
 
       });
